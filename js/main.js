84b0fa688d595c3c8f3e551ef625d73b02137dfe
Myakish <myakishevandrey@gmail.com>
2021-12-18 14:42:28 +1000

    Сверстанно мобильное меню

--- js/main.js
+++ js/main.js
@@ -13,3 +13,10 @@ const swiper = new Swiper(".swiper", {
 
   // And if we need scrollbar
 });
+
+const menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", () => {
+  document
+    .querySelector(".navbar-bottom")
+    .classList.toggle("navbar-bottom--visible");
+});
