0f3c874f5abf56a3c4099a5654c79716e366d25d
Myakish <myakishevandrey@gmail.com>
2021-12-18 22:50:04 +1000

    добавленны модальные окна

--- js/main.js
+++ js/main.js
@@ -20,3 +20,52 @@ menuButton.addEventListener("click", () => {
     .querySelector(".navbar-bottom")
     .classList.toggle("navbar-bottom--visible");
 });
+
+const bookingkBtn = document.querySelector(".booking__button");
+const modalOverlay = document.querySelector(".modal__overlay");
+const modalDiaglog = document.querySelector(".modal__dialog");
+const closeBtnModal = document.querySelector(".button__close");
+const cardBooks = document.querySelectorAll(".card__button");
+//Поиск
+const searchBtn = document.querySelector(".search__button");
+const searchValue = document.querySelector(".search__input");
+//форма
+const form = document.querySelector(".form");
+
+for (let cardBook of cardBooks) {
+  cardBook.addEventListener("click", (event) => {
+    modalStyles("1", "visible");
+  });
+}
+
+bookingkBtn.addEventListener("click", () => {
+  modalStyles("1", "visible");
+});
+
+closeBtnModal.addEventListener("click", () => {
+  modalStyles("0", "hidden");
+});
+
+function modalStyles(opasity, visibility) {
+  modalOverlay.style.opacity = opasity;
+  modalOverlay.style.visibility = visibility;
+  modalDiaglog.style.opacity = opasity;
+  modalDiaglog.style.visibility = visibility;
+}
+//форма
+searchBtn.addEventListener("click", (event) => {
+  console.log(event);
+  searchValue.value = "";
+});
+searchValue.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    searchValue.value = "";
+  }
+  console.log(event);
+});
+form.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    searchValue.value = "";
+  }
+  console.log(event);
+});
