cbc5c8d4264d6fc89b064e46e45ceacc7930e8b9
Myakish <myakishevandrey@gmail.com>
2022-02-02 09:05:48 +1000

    add section-slider

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper(".swiper", {
+const swiper = new Swiper('.swiper-conteiner', {
   // Optional parameters
   // direction: "horizontal",
   loop: true,
@@ -7,28 +7,45 @@ const swiper = new Swiper(".swiper", {
 
   // Navigation arrows
   navigation: {
-    nextEl: ".slider-button--next",
-    prevEl: ".slider-button--prev",
+    nextEl: '.slider-button--next',
+    prevEl: '.slider-button--prev',
+  },
+  effect: 'coverflow',
+
+  // And if we need scrollbar
+});
+
+const reviews = new Swiper('.article-reviews__slider', {
+  // Optional parameters
+  // direction: "horizontal",
+  loop: true,
+  speed: 500,
+  // If we need pagination
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.article-reviews__button-next',
+    prevEl: '.article-reviews__button-prev',
   },
 
   // And if we need scrollbar
 });
 
-const menuButton = document.querySelector(".menu-button");
+const menuButton = document.querySelector('.menu-button');
 const widthWindow = window.outerWidth;
 const windowInnerWidth = window.innerWidth;
 let widthScrollBar = widthWindow - windowInnerWidth;
-const header = document.querySelector(".navbar--mobille_fixed");
-menuButton.addEventListener("click", () => {
-  const mobileMenu = document.querySelector(".navbar-bottom");
-  mobileMenu.classList.toggle("navbar-bottom--visible");
+const header = document.querySelector('.navbar--mobille_fixed');
+menuButton.addEventListener('click', () => {
+  const mobileMenu = document.querySelector('.navbar-bottom');
+  mobileMenu.classList.toggle('navbar-bottom--visible');
 
-  if (mobileMenu.classList.contains("navbar-bottom--visible")) {
-    body.style.overflow = "hidden";
+  if (mobileMenu.classList.contains('navbar-bottom--visible')) {
+    body.style.overflow = 'hidden';
     body.style.paddingRight = `${widthScrollBar}px`;
     header.style.paddingRight = `${widthScrollBar}px`;
   } else {
-    body.style.overflow = "";
+    body.style.overflow = '';
     body.style.paddingRight = ``;
     header.style.paddingRight = ``;
   }
@@ -39,32 +56,32 @@ console.log(widthWindow);
 console.log(windowInnerWidth);
 console.log(widthScrollBar);
 
-const bookingkBtn = document.querySelector(".booking__button");
+const bookingkBtn = document.querySelector('.booking__button');
 
-const modalOverlay = document.querySelector(".modal__overlay");
-const modalDiaglog = document.querySelector(".modal__dialog");
-const closeBtnModal = document.querySelector(".button__close");
-const cardBooks = document.querySelectorAll(".card__button");
-const body = document.querySelector("body");
+const modalOverlay = document.querySelector('.modal__overlay');
+const modalDiaglog = document.querySelector('.modal__dialog');
+const closeBtnModal = document.querySelector('.button__close');
+const cardBooks = document.querySelectorAll('.card__button');
+const body = document.querySelector('body');
 //Поиск
-const searchBtn = document.querySelector(".search__button");
-const searchValue = document.querySelector(".search__input");
+const searchBtn = document.querySelector('.search__button');
+const searchValue = document.querySelector('.search__input');
 //форма
-const form = document.querySelector(".form");
+const form = document.querySelector('.form');
 
 for (let cardBook of cardBooks) {
-  cardBook.addEventListener("click", (event) => {
-    modalStyles("1", "visible");
+  cardBook.addEventListener('click', event => {
+    modalStyles('1', 'visible');
   });
 }
 
-bookingkBtn.addEventListener("click", () => {
-  modalStyles("1", "visible");
+bookingkBtn.addEventListener('click', () => {
+  modalStyles('1', 'visible');
 });
 
-closeBtnModal.addEventListener("click", () => {
-  modalStyles("0", "hidden");
-  body.style.overflow = "";
+closeBtnModal.addEventListener('click', () => {
+  modalStyles('0', 'hidden');
+  body.style.overflow = '';
 });
 
 function modalStyles(opasity, visibility) {
@@ -74,19 +91,19 @@ function modalStyles(opasity, visibility) {
   modalDiaglog.style.visibility = visibility;
 }
 //форма
-searchBtn.addEventListener("click", (event) => {
+searchBtn.addEventListener('click', event => {
   console.log(event);
-  searchValue.value = "";
+  searchValue.value = '';
 });
-searchValue.addEventListener("keydown", (event) => {
-  if (event.key === "Enter") {
-    searchValue.value = "";
+searchValue.addEventListener('keydown', event => {
+  if (event.key === 'Enter') {
+    searchValue.value = '';
   }
   console.log(event);
 });
-form.addEventListener("keydown", (event) => {
-  if (event.key === "Enter") {
-    searchValue.value = "";
+form.addEventListener('keydown', event => {
+  if (event.key === 'Enter') {
+    searchValue.value = '';
   }
   console.log(event);
 });
